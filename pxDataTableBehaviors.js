00e8ce19ad07139cf38bdbed68ef8c3e438c29d6
Martin Wragg <martin.wragg@ge.com>
2015-09-15 14:56:03 -0700

    Per column filtering.

--- pxDataTableBehaviors.js
+++
@@ -1,7 +0,0 @@
-pxDataTableBehaviors = {
-  sort: function(a, b) {
-    return this.descending
-    ? (a.value < b.value ? 1 : -1)
-    : (a.value > b.value ? 1 : -1);
-  }
-};
---
+++ pxDataTableBehaviorsDemo.js
@@ -0,0 +1,13 @@
+var pxDataTableBehaviors = {
+  sortColumn: function(a, b) {
+    return this.descending
+    ? (a.value < b.value ? 1 : -1)
+    : (a.value > b.value ? 1 : -1);
+  },
+  filterMarkupVal: function(searchString, cellValue){
+    var lhs = cellValue.slice(cellValue.indexOf('>') + 1);
+    var content = lhs.substr(0 ,lhs.indexOf('<'));
+
+    return (content.toString().toLowerCase().indexOf(searchString.toString().toLowerCase()) > -1);
+  }
+};
