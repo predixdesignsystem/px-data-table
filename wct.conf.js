5341ccc759039d1ed64fb87958399900ea065d18
gedesigntech <henry.navarro@ge.com>
2015-12-02 11:31:43 -0800

    Added more tests

---
+++ wct.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+    verbose: true,
+    plugins: {
+        local: {
+            browsers: ['chrome', 'firefox']
+        },
+        sauce: {
+            disabled: true
+        }
+    }
+};
