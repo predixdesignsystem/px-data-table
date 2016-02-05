61aaeb640e8e88b5c1d71f7b257d7cc39d27ba95
Martin Wragg <mdwragg@gmail.com>
2016-02-05 11:58:35 -0800

    fix tests across firefox/safari.

--- test/px-data-table-test.js
+++ test/px-data-table-test.js
@@ -835,14 +835,26 @@ function runTests() {
     var tb = Polymer.dom(table5Fixture.root).querySelector('aha-table'),
         cell = Polymer.dom(tb.root).querySelectorAll('.aha-last-td')[0];
 
-    cell.addEventListener('click', function(){
-      assert.equal(getStyle(this, 'background-color'), 'rgb(239, 239, 244)');
-      assert.equal(getStyle(this, 'border'), '1px solid rgb(62, 135, 232)');
-      assert.equal(getStyle(this, 'box-shadow'), 'rgb(228, 228, 234) 0px 0px 0px 1px inset');
+    cell.click();
+
+    flush(function(){
+      assert.equal(getStyle(cell, 'background-color'), 'rgb(239, 239, 244)');
+      assert.equal(getStyle(cell, 'border-right-width'), '1px');
+      assert.equal(getStyle(cell, 'border-right-style'), 'solid');
+      assert.equal(getStyle(cell, 'border-right-color'), 'rgb(62, 135, 232)');
+      assert.equal(getStyle(cell, 'box-shadow'), 'rgb(228, 228, 234) 0px 0px 0px 1px inset');
 
       done();
     });
-    cell.click();
+
+
+    // cell.addEventListener('click', function(){
+    //   assert.equal(getStyle(this, 'background-color'), 'rgb(239, 239, 244)');
+    //   assert.equal(getStyle(this, 'border'), '1px solid rgb(62, 135, 232)');
+    //   assert.equal(getStyle(this, 'box-shadow'), 'rgb(228, 228, 234) 0px 0px 0px 1px inset');
+    //
+    //   done();
+    // });
   });
 
   });
