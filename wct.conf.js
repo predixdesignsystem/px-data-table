db5310f8c0a29e35776d19e35d6bbe7b17f4e949
Martin Wragg <mdwragg@gmail.com>
2016-03-14 22:36:32 -0700

    Testing travis config

--- wct.conf.js
+++ wct.conf.js
@@ -1,14 +1,34 @@
 module.exports = {
-    verbose: false,
-    plugins: {
-        local: {
-            browsers: ['chrome', 'firefox']
-        },
-        sauce: {
-            disabled: true
-        }
+  verbose: false,
+  plugins: {
+    local: {
+        browsers: ['chrome', 'firefox']
     },
-    suites: [
-      'test/px-data-table-fixture.html'
-    ]
+    sauce: {
+      disabled: true,
+      "browsers": [{
+          "browserName": "microsoftedge",
+          "platform": "Windows 10",
+          "version": ""
+        }, {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.11",
+          "version": "9"
+        }, {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        }
+      ]
+    }
+  },
+  suites: [
+    'test/px-data-table-fixture.html'
+  ]
 };
