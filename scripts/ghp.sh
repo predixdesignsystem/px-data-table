1978bd43c6d44dc0e1a5f17853c9fb71a25fa581
Martin Wragg <mdwragg@gmail.com>
2016-07-26 15:01:09 -0700

    optimise gh-pages demo build.

--- scripts/ghp.sh
+++ scripts/ghp.sh
@@ -42,6 +42,14 @@ yes | cp ../tmp_bower/bower.json bower.json
 #install your new tag through bower, it will fail without forcing it.
 bower install ${REPO_NAME} --force
 
+#optimize for production
+cd ${REPO_NAME} #go into the component folder
+npm install vulcanize
+vulcanize index.html -o index.vulacanized.html --inline-scripts --inline-css --strip-comments
+yes | cp index.vulacanized.html index.html
+rm index.vulacanized.html
+cd ../ #remember to exit out of the component before you do any git stuff
+
 #do the git stuff
 git add .
 git commit -m "rebuild github pages"
