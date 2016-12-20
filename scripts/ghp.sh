cb3c8902a86d22435b95c3eeed6a173dcb953249
David Leonard <davidrobertleonard@gmail.com>
2016-12-20 14:32:38 -0800

    Update to build pipeline

--- scripts/ghp.sh
+++ scripts/ghp.sh
@@ -16,13 +16,14 @@ REPO=`git config remote.origin.url`
 # Prep git credentials
 GIT_USER_NAME="Travis CI"
 GIT_USER_EMAIL="PredixtravisCI@ge.com"
+GIT_COMMIT_MESSAGE="[Travis] Rebuild documentation for Github Pages"
 
 # Check if we should run a deploy, or if we should skip it. Only commits to master
 # should trigger a build. Pull requests and commits to features branches should not.
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    exit 0
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#     echo "Skipping deploy; just doing a build."
+#     exit 0
+# fi
 
 # ------------------------------------------------------------------------------
 # PREPARE FILESYSTEM
@@ -46,9 +47,21 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 # ... and copy the bower.json file from our temp directory into the current one, overriding it, and passing a yes in there's a prompt
 yes | cp ../tmp_bower/bower.json bower.json
 
+# Overwrite whatever is in root .bowerrc to force installation of bower packages at the root
+rm -f .bowerrc
+echo "{ \"directory\": \".\" }" > .bowerrc
+
+# Overwrite whatever is in root `index.html` to create the redirect
+# Note: We are not overwriting the component's documentation `index.html` file
+# here, we are making sure that http://url/px-something/ redirects to
+# http://url/px-something/px-something/, where the demo page is installed
+rm -f index.html
+meta_temp='<META http-equiv=refresh content="0;URL=COMPONENT_NAME/">'
+echo ${meta_temp/'COMPONENT_NAME'/$REPO_NAME} > index.html
+
 # Install your new tag through bower (use --force because it will fail without forcing it)
 bower install ${REPO_NAME} --force
-# Force install px-dark-theme (to generate dark-theme demos)
+# @DARK_THEME: Force install px-dark-theme (to generate dark-theme demos)
 bower install px-dark-theme --force
 
 # ------------------------------------------------------------------------------
@@ -58,32 +71,11 @@ bower install px-dark-theme --force
 # Go into the component folder we've just installed from bower
 cd ${REPO_NAME}
 
-# Install vulcanize
-npm install vulcanize -g
+# @DARK_THEME: Copy `index.html` to new file, where we will add dark theme
+yes | cp index.html index-dark.html
 
-# Run vulcanize to prepare files for production
-vulcanize index.html -o index.vulcanized.html --inline-scripts --inline-css --strip-comments
-
-# Move original index to `indexh2` and prepapre an `indexdarkh2` for dark-theme
-yes | cp index.html indexh2.html
-yes | cp indexh2.html indexdarkh2.html
-
-# Import dark-theme on the `indexdarkh2` page
-perl -pi -w -e 's/px-theme\/px-theme-styles.html/px-dark-theme\/px-dark-theme-styles.html/g;' indexdarkh2.html
-
-# Move vulcanized `index` file to `index.html`, overwrite existing
-yes | cp index.vulcanized.html index.html
-rm index.vulcanized.html
-
-# -------------------------- SPECIFIC TO THIS PROJECT --------------------------
-
-# Vulcanize additional pages (i.e. component demo pages)
-vulcanize px-data-table-column-demo.html -o px-data-table-column-demo.vulcanized.html --inline-scripts --inline-css --strip-comments
-yes | cp px-data-table-column-demo.vulcanized.html px-data-table-column-demo.html
-rm px-data-table-column-demo.vulcanized.html
-vulcanize px-data-table-highlight-demo.html -o px-data-table-highlight-demo.vulcanized.html --inline-scripts --inline-css --strip-comments
-yes | cp px-data-table-highlight-demo.vulcanized.html px-data-table-highlight-demo.html
-rm px-data-table-highlight-demo.vulcanized.html
+# @DARK_THEME: Import dark-theme on the `index-dark.html` page
+perl -pi -w -e 's/px-theme\/px-theme-styles.html/px-dark-theme\/px-dark-theme-styles.html/g;' index-dark.html
 
 # ------------------------------------------------------------------------------
 # SW-PRECACHE
@@ -99,35 +91,30 @@ rm px-data-table-highlight-demo.vulcanized.html
 # Remember to exit out of the component before we do any git stuff
 cd ../
 
+# Do the git stuff
+git add .
+git commit -m ${GIT_COMMIT_MESSAGE}
+
 # Set git credentials (defined in settings above)
 git config user.name ${GIT_USER_NAME}
 git config user.email ${GIT_USER_EMAIL}
 
-# Parse https remote URL, find-and-replace to create a SSH remote URL we can
-# push to for PredixDev GH organization
-SSH_PREDIXDEV_REMOTE=${REPO/https:\/\/github.com\//git@github.com:}
+# We get the URL in this format: "https://github.com/PredixDev/px-something"
+# First, we need to replace https-style remote URL with a SSH-style remote
+# URL we can push to below
+SSH_GIT=${REPO/https:\/\/github.com\//git@github.com:}
 
-# Replace `PredixDev` organization with `predix-ui` to create an additional
-# remote we will push to for second docs repo
-SSH_PREDIXUI_REMOTE=${SSH_PREDIXDEV_REMOTE/:PredixDev\//:predix-ui\/}
-
-# Do the git stuff
-git add .
-git commit -m "[Travis] Automatically rebuild documentation for Github Pages"
+# Now, the URL is in this format: "git@github.com:PredixDev/px-something"
+# Next, replace `PredixDev` Github organization with `predix-ui` so configure
+# the correct remote to push to.
+# The resulting URL will be: "git@github.com:predix-ui/px-something"
+SSH_GIT_PREDIXUI=${SSH_GIT/:PredixDev\//:predix-ui\/}
 
 # Prepare ssh key, which we'll use to authenticate for SSH-push deploy
 eval `ssh-agent -s`
 # ... and change permissions for deploy key
-chmod 0400 $TRAVIS_BUILD_DIR/deploy_key1
-chmod 0400 $TRAVIS_BUILD_DIR/deploy_key2
+chmod 0400 $TRAVIS_BUILD_DIR/deploy_key
 
-# First, push to PredixDev/REPO (force to override out-of-date refs)
-ssh-add $TRAVIS_BUILD_DIR/deploy_key1
-git push $SSH_PREDIXDEV_REMOTE $TARGET_BRANCH --force
-
-# Second, remove the previous identity.
-ssh-add -D
-
-# Third, push to predix-ui/REPO (force to override out-of-date refs)
-ssh-add $TRAVIS_BUILD_DIR/deploy_key2
-git push $SSH_PREDIXUI_REMOTE $TARGET_BRANCH --force
+# Push to predix-ui/repo `gh-pages` branch (force to override out-of-date refs)
+ssh-add $TRAVIS_BUILD_DIR/deploy_key
+git push $SSH_GIT_PREDIXUI $TARGET_BRANCH --force
