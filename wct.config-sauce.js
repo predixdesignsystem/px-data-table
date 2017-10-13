e07eb60441e3f151daad9ff0ef6e87b54b0c5b86
Evan Dana <evan.dana@gmail.com>
2017-10-13 11:47:31 -0400

    px-pagination not showing up

--- wct.config-sauce.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-    sauceUsername: 'bchange',
-    sauceAccessKey: 'dd1c7254-4be1-4dc4-a03b-3cf0e1ef3e61',
-    sauceSeleniumAddress: '3.26.203.223:4445/wd/hub',
-    sauceSeleniumUseHttp: true,
-    sauceTunnelId: '4a0b420999e84939acf2a0790ef7a29e',
-    name: 'MAKENAMEDYNAMIC-Chrome61-Windows10',
-};
